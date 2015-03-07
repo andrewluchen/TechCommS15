bb76d7e89e28443c464684dccd9d5cca3ee45ea9
Andrew Chen <andch43212@gmail.com>
2015-03-07 05:11:52 -0500

    playing with html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from flask_oauth import OAuth
 import os
 
